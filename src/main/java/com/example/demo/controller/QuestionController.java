5b3e32e8a4c54d2280e33e8a84aabce10aac2708
heowords <1521552570@qq.com>
2022-03-17 16:02:47 +0800

    Changes

--- src/main/java/com/example/demo/controller/QuestionController.java
+++ src/main/java/com/example/demo/controller/QuestionController.java
@@ -1,9 +1,14 @@
 package com.example.demo.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.demo.GA.Question;
+import com.example.demo.Response.CommonReturnType;
+import com.example.demo.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +21,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/question")
 public class QuestionController {
+    @Autowired
+    QuestionService questionService;
 
+    //fillQuestion
+    //添加成功
+    @PostMapping("/save")
+    public CommonReturnType saveQuestion(@RequestBody Question q ){
+        questionService.save(q);
+        return CommonReturnType.create(null);
+    }
+    //查询成功
+    @PostMapping ("/get")
+    public CommonReturnType getAllQuesion(@RequestParam String phone){
+
+        List<Question> data=questionService.list(new QueryWrapper<Question>()
+                .eq("userId", phone)
+        );
+
+        return CommonReturnType.create(data);
+    }
+
+    //删除成功
+    @PostMapping ("/remove")
+    public CommonReturnType removeQuestion(@RequestParam int id){
+
+        boolean data=questionService.remove(new QueryWrapper<Question>()
+                .eq("userId", id)
+        );
+        if(data==false){
+            return CommonReturnType.create("没有该题目或已经被删除");
+        }
+
+        return CommonReturnType.create(null);
+    }
 }
 
--- src/main/java/com/example/demo/dao/userLogonDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import com.example.demo.entity.Userlogon;
-
-
-public interface userLogonDao {
-    // 查询ID、密码、登录状态匹配；修改密码：退出登录
-    Userlogon selectById(String Id);
-}
