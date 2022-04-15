3b4f37eef7f2cc532665238115296a8d84530395
heowords <1521552570@qq.com>
2022-04-15 14:09:51 +0800

    Changes

--- src/main/java/com/example/demo/controller/GetFileController.java
+++ src/main/java/com/example/demo/controller/GetFileController.java
@@ -38,7 +38,8 @@ public class GetFileController {
     @PostMapping("/excel/{phone}/{subject}/{examcode}")
     public CommonReturnType savefile(@RequestBody MultipartFile file,@PathVariable String phone,@PathVariable String subject,@PathVariable Integer examcode){
         log.info(String.valueOf(file));
-        File tfile = new File(file.getOriginalFilename());
+//        File tfile = new File(file.getOriginalFilename());
+        File tfile =MultipartFileToFile(file);
         XSSFWorkbook workbook=new XSSFWorkbook(FileUtils.openInputStream(tfile));
         XSSFSheet sheet=workbook.getSheetAt(0);
         //获取sheet中最后一行行号
@@ -69,47 +70,48 @@ public class GetFileController {
             }
             scoreService.save(score);
         }
+        tfile.delete();
         return CommonReturnType.create(null);
     }
-//    public static File MultipartFileToFile(MultipartFile multipartFile) {
-//
-//        File file = null;
-//        //判断是否为null
-//        if (multipartFile.equals("") || multipartFile.getSize() <= 0) {
-//            return file;
-//        }
-//        //MultipartFile转换为File
-//        InputStream ins = null;
-//        OutputStream os = null;
-//        try {
-//            ins = multipartFile.getInputStream();
-//            file = new File(multipartFile.getOriginalFilename());
-//            os = new FileOutputStream(file);
-//            int bytesRead = 0;
-//            byte[] buffer = new byte[8192];
-//            while ((bytesRead = ins.read(buffer, 0, 8192)) != -1) {
-//                os.write(buffer, 0, bytesRead);
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }finally {
-//            if(os != null){
-//                try {
-//                    os.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//            if(ins != null){
-//                try {
-//                    ins.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
-//        return file;
-//    }
+    public static File MultipartFileToFile(MultipartFile multipartFile) {
+
+        File file = null;
+        //判断是否为null
+        if (multipartFile.equals("") || multipartFile.getSize() <= 0) {
+            return file;
+        }
+        //MultipartFile转换为File
+        InputStream ins = null;
+        OutputStream os = null;
+        try {
+            ins = multipartFile.getInputStream();
+            file = new File(multipartFile.getOriginalFilename());
+            os = new FileOutputStream(file);
+            int bytesRead = 0;
+            byte[] buffer = new byte[8192];
+            while ((bytesRead = ins.read(buffer, 0, 8192)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(os != null){
+                try {
+                    os.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(ins != null){
+                try {
+                    ins.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return file;
+    }
 
 
 
