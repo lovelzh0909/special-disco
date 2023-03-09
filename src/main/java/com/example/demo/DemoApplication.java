eeb9ece0f0e42b83b4048f0a03baf2eac6f1fb1e
follow <follow@qq.com>
2023-03-09 17:27:50 +0800

    First commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,23 +1,12 @@
 package com.example.demo;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
 
 @SpringBootApplication
-@EnableScheduling
-
 public class DemoApplication {
-
-	public static void main(String[] args) {
-		// System.out.println("\n Look \n\n");
-		SpringApplication.run(DemoApplication.class, args);
-		// ConfigurableApplicationContext run=
-		// ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeanConfig.class);
-		// System.out.println(run.getBean("user",User.class));
-
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,6 @@
+package com.example.demo;
+public class HelloController {
+    public String Hello(){
+        return "Hello spring boot!";
+    }
+}
